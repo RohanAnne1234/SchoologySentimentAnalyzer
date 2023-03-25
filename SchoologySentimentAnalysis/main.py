20a434946213bd4430ba2636e839568581b5a623
Rohan Anne <59780336+RohanAnne1234@users.noreply.github.com>
2023-03-25 10:08:06 -0400

    Initial Commit
    Good luck!

---
+++ SchoologySentimentAnalysis/main.py
@@ -0,0 +1,26 @@
+# This is a sample Python script.
+
+import tkinter as tk
+
+import schoology_interface
+import title_screen
+
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def main():
+    window = tk.Tk()
+    window.mainloop()
+    schoology_interface.main()
+    title_screen.main()
+
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    main()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
